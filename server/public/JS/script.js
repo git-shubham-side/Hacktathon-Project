b24657ce4fcf70b6f25fe878bfa6162bc570b11b
Shubh <rathodshubham7711@gmail.com>
2026-03-28 23:39:46 +0530

    client folder delete

--- server/public/JS/script.js
+++ server/public/JS/script.js
@@ -1,16 +1,11 @@
-
 function trimForm() {
   let inputs = document.querySelectorAll("input");
 
-  inputs.forEach(input => {
+  inputs.forEach((input) => {
     let type = input.type;
 
     // Only trim text-based inputs
-    if (
-      type === "text" ||
-      type === "email" ||
-      type === "password"
-    ) {
+    if (type === "text" || type === "email" || type === "password") {
       input.value = input.value.trim();
     }
   });
